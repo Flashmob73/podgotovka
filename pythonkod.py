2bf8a3129f5643d26d67aa94498d3dbff7ac5678
Dinar Nurullin <36741206+dinnur73@users.noreply.github.com>
2018-05-04 22:39:29 +0400

    Update pythonkod.py

--- pythonkod.py
+++ pythonkod.py
@@ -1,7 +1,8 @@
 import activistov instagram.com : @dinnur73 , @barkhaevaa, @ksucurly. #исследования 
 add picca
 find mistakes
-FBUZ talk 
+FBUZ talk
+анонс flashmob73
 
 
 Create company https://sme.tinkoff.ru/accounts
